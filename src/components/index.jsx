52cf81befa0a8b410a2f465d6e63111662100de7
mansha <nigwal.1998@gmail.com>
2024-02-21 01:48:56 +0530

    creating login page in Task Manager project

---
+++ src/components/index.jsx
@@ -0,0 +1,7 @@
+import Button from "./Button";
+import Textbox from "./Textbox";
+
+export {
+    Button,
+    Textbox
+}
---
+++ src/pages/index.jsx
@@ -0,0 +1,15 @@
+import Task from "./Task";
+import Login from "./Login";
+import Dashboard from "./Dashboard";
+import TaskDetails from "./TaskDetails";
+import Trash from "./Trash";
+import Users from "./Users";
+
+export {
+    Task,
+    Login,
+    Dashboard,
+    TaskDetails,
+    Trash,
+    Users
+}
